44fa2c25ee75a58162dc13213b86943a4c7462f4
MasterItaly <ItalHD@web.de>
2023-12-01 08:29:06 +0100

    Reset

--- my-angular-app/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-  production: false, // Set to true for production
-  apiUrl: 'https://your-api-url.com', // Your API URL
-  // Add other environment variables as needed
-};
--- my-angular-app/src/main.ts
+++ my-angular-app/src/main.ts
@@ -1,13 +1,6 @@
-// src/main.ts
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment'; // Ensure the correct path
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
